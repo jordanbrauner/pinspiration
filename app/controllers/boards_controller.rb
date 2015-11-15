ef39494dbb500fc514c95fa56d40c7dd790919fa
James Moon <mooniker@gmail.com>
2015-11-15 18:36:36 -0500

    Add board model, controller, and filler views, tweak styling

---
+++ app/controllers/boards_controller.rb
@@ -0,0 +1,48 @@
+class BoardsController < ApplicationController
+
+  def index
+    @boards = Board.all
+  end
+
+  def new
+    @board = Board.new
+  end
+
+  def create
+    @board = Board.new(board_params)
+    if @board.save
+      redirect_to boards_path
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @board = Board.find(params[:id])
+  end
+
+  def edit
+    @board = Board.find(params[:id])
+  end
+
+  def update
+    @board = Board.find(params[:id])
+    if @board.update(board_params)
+      redirect_to boards_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @board = Board.find(params[:id])
+    @board.destroy
+    redirect_to boards_path
+  end
+
+  private
+    def board_params
+      params.require(:board).permit(:title)
+    end
+
+end
